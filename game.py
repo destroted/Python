1523ac4d79050240a1594d7f9872d138be2ba38c
plaidypus <dudey405@gmail.com>
2014-11-26 23:31:52 -0500

    Created a while loop; added a new task

--- game.py
+++ game.py
@@ -1,4 +1,42 @@
-game = 5;
-print game
-print game + 5
-print "Let's do this!"
+# This is a valid comment. Make sure to use the '#' key
+
+# Create a main menu with three options:
+# 1. Print the product of 3 x 4
+# 2. Take in user name, and add 'is gay' at the end
+# 3. Quit
+
+def mainMenu(): #Need colon at end
+	print "******************"
+	print "** A&V Programs **"
+	print '**              **' #Notice the single quotes? They still work
+	print "** 1. Print 3x4 **"
+	print "** 2. User Name **"
+	print "** 3. Quit      **"
+	print "******************"
+
+def calculation():
+	print 3 * 4
+
+def isGay(name):
+	print name + " is gay!" #Concatenation right here
+
+
+isRunning = 1
+
+while (isRunning == 1):
+	mainMenu()
+	userInput = input("Enter a number: ") #Need to user input() **NOT RAW_INPUT** for anything other than string
+	if (userInput > 3 and userInput < 1):
+		print "Incorrect response. Try again."
+	elif (userInput == 1):
+		calculation()
+	elif (userInput == 2):
+		print "Enter your name: "
+		userName = raw_input() #Input as a string
+		isGay(userName)
+	elif (userInput == 3):
+		print "Goodbye!"
+		isRunning = 0
+
+
+
--- readme.txt
+++ readme.txt
@@ -5,6 +5,34 @@ Adam:
 
 -created makefile
 -started the readme
--to run files, just type "make", and the program 
-	will automatically run, printing 
-	two numbers and a string
+-to run files, just type "make", and 
+	the program will automatically run, 
+	printing two numbers and a string.
+
+
+****************************
+*** Wed,  Nov  26th 2014 ***
+****************************
+Adam:
+
+-to run program, while in Python directory, 
+	type 'make'.
+-a menu will appear with three options
+	and a prompt telling you to enter a
+	number.
+-first option will print out the product
+	of 3x4
+-second option will ask the user for the
+	user's name, then concatenate "is gay"
+	to the user's name. It will be printed
+-third option quits the loop
+-raw_input() is for string only
+ 	use: input() for any other data type
+ -comments are used
+-multiple functions are used
+-------------
+- NEW TASK: -
+-------------
+- Create a program that takes in 5 numbers from
+	the user in a for loop. Add up all 5 numbers
+	and print the average. 
